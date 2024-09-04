f5d413485d482f39b03cd1dc16203a5db3fad4dc
giftedpromise <giftedpromise1992@gmail.com>
2024-09-04 15:38:37 +0100

    edit

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -11,7 +11,8 @@ import {
 export const ToDoList = () => {
   const [items, setItems] = useState([]);
   const [newItem, setNewItem] = useState("");
- 
+  const [editIndex, setEditIndex] = useState(null);
+  const [editValue, setEditValue] = useState("");
 
   // Add new item to the list
   const addItem = () => {
